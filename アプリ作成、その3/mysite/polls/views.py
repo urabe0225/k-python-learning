eb7230d565e81ad2a2ccf6bee6038a38a9c4ff81
hiromu-bit <hiromu0621@icloud.com>
2023-08-09 14:46:52 +0900

    ビューの追加

--- アプリ作成、その3/mysite/polls/views.py
+++ アプリ作成、その3/mysite/polls/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list,}
+    return HttpResponse(template.render(context, request))
+    #output = ", ".join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    #return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
 
 def detail(request, question_id):
