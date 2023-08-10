daec01da04d51ab60a7d95e4550106a340fcd01c
hiromu-bit <hiromu0621@icloud.com>
2023-08-10 14:10:09 +0900

    汎用ビューの使用

--- アプリ作成、その4/mysite/polls/views.py
+++ アプリ作成、その4/mysite/polls/views.py
@@ -1,34 +1,53 @@
-from django.http import Http404
+#from django.http import Http404
+from django.views import generic
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 #from django.template import loader
 
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+#def index(request):
+#    latest_question_list = Question.objects.order_by("-pub_date")[:5]
     #template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+#    context = {"latest_question_list": latest_question_list}
+#    return render(request, "polls/index.html", context)
     #return HttpResponse(template.render(context, request))
     #output = ", ".join([q.question_text for q in latest_question_list])
     #return HttpResponse(output)
     #return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+#def detail(request, question_id):
+  #  question = get_object_or_404(Question, pk=question_id)
     #try:
         #question = Question.objects.get(pk=question_id)
     #except Question.DoesNotExist:
         #raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question":question})
+ #   return render(request, "polls/detail.html", {"question":question})
     #return HttpResponse("You're looking at the results of question %s." % question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, "polls/results.html", {"question": question})
     #response = "You're looking at the results of question %s."
     #return HttpResponse(response % question_id)
 
