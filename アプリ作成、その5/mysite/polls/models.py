5670ba8fd95922d507ebe5e0754e8f7c0e0847ee
hiromu-bit <hiromu0621@icloud.com>
2023-08-17 11:24:56 +0900

    models.pyの変更

--- アプリ作成、その5/mysite/polls/models.py
+++ アプリ作成、その5/mysite/polls/models.py
@@ -1,17 +1,23 @@
+
+# Create your models here.
+
 import datetime
 
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(date=1)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)    
+    
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -19,4 +25,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-    
