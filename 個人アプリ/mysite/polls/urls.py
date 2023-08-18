4170536f2f90326bcbc401ee06b73d2b9e058ecc
hiromu-bit <hiromu0621@icloud.com>
2023-08-18 16:38:56 +0900

    個人アプリのフォルダ作成

--- 個人アプリ/mysite/polls/urls.py
+++ 個人アプリ/mysite/polls/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
-from .views import TodoList
 
+from . import views
+
+app_name = "polls"
 urlpatterns = [
-    path('list/', TodoList.as_view(), name='list'),
-]
-
-#from . import views
-
-#app_name = "polls"
-#urlpatterns = [
-#    path("", views.IndexView.as_view(), name="index"),
-#    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-#    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-#    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 
     # ex: /polls/
     #path("", views.index, name="index"),
@@ -22,4 +17,4 @@ urlpatterns = [
     #path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
     #path("<int:question_id>/vote/", views.vote, name="vote"),
-#]
+]
